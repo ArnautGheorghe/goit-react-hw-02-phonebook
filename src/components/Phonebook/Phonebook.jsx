43a24ae49edba2ce1a3e1263814657e8a9b6c3e7
ArnautGheorghe <agigi94@yahoo.com>
2024-02-15 19:10:52 +0200

    tema 2 ex 2

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,24 +1,32 @@
 
 import { Component } from "react";
-
+import PropTypes from 'prop-types';
 import Button from "../Button/Button";
 
 
 export default class Phonebook extends Component {
+  static propTypes = { onFormSubmit: PropTypes.func };
   state = {
+    isAddFormVisible: false,
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: '',
+    filter: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
   };
 
   handleSubmit = e => {
     e.preventDefault();
+    this.props.onFormSubmit(this.state);
   };
 
   handleChange = evt => {
@@ -27,8 +35,10 @@ export default class Phonebook extends Component {
     console.log('change');
   };
 
+ 
+
   render() {
-    const { name, number, isAddFormVisible } = this.state;
+    const { name, number, isAddFormVisible, finder } = this.state;
 
     return (
       <div>
@@ -73,56 +83,80 @@ export default class Phonebook extends Component {
             Add Contact
           </Button>
 
-          {console.log(this.state)}
+          {console.log(this.setState)}
         </form>
+
         <h1>Contacts</h1>
-        {isAddFormVisible && (
-          <span>
-            {this.state.name} : {this.state.number}
-          </span>
-        )}
         <div>
-          {this.render2()}
+          <h2>Find contacts by name</h2>
+          <input
+            type="text"
+            value={finder}
+            name="finder"
+            placeholder="Finder"
+            onChange={this.handleChange}
+            onClick={this.handleFind}
+          />
         </div>
+        {isAddFormVisible &&
+          (this.state.filter.forEach(el => el.name) !== finder ? (
+            <ul>
+              <li key={this.state.name.length}>{'ok'}</li>
+            </ul>
+          ) : (
+            <ul>
+              <li>{'rEzultat eronat'}</li>
+            </ul>
+          ))}
+        {isAddFormVisible && (
+          <ul>
+            <li key={this.state.name.length}>
+              <span>
+                {this.state.name} : {this.state.number}
+              </span>
+            </li>
+          </ul>
+        )}
+
+        <div>{this.render2()}</div>
       </div>
     );
   }
-
   renderList = contacts => {
     return contacts.map(el => {
       return (
-        <div key={el.id}>
-          <div>
-            {el.name} :{el.number}
-          </div>
-        </div>
+        <ul>
+          <li key={el.id}>
+            <div>
+              {el.name} :{el.number}
+            </div>
+          </li>
+        </ul>
       );
     });
   };
 
   render2() {
     const { contacts } = this.state;
-    return (
-        <div>{this.renderList(contacts)}</div>
-      
-    );
+    return <div>{this.renderList(contacts)}</div>;
   }
 
   handleTutor = data => {
-    const newId = this.state.contacts.length > 0 ? this.state.contacts.length : 0;
+    const newId =
+      this.state.contacts.length > 0 ? this.state.contacts.length : 0;
 
     const tutorToAdd = {
       id: newId,
       name: data.name,
-      number: data.phone,
-      };
+      number: data.number,
+    };
 
     /**
      * Pentru orice state care este un obiect sau array si avem nevoie de starea precedenta, folosim metoda de mai jos
      */
     this.setState(prevState => {
       return {
-        list: [...prevState.list, tutorToAdd],
+        contacts: [...prevState.contacts, tutorToAdd],
         isAddFormVisible: false,
       };
     });
